05e8ca45414d1aa123fca4685a65ae49dc838a4c
Sontome <93179211+Sontome@users.noreply.github.com>
2026-03-15 07:30:53 +0700

    Update VNABookingModal.tsx

--- src/components/VNABookingModal.tsx
+++ src/components/VNABookingModal.tsx
@@ -338,7 +338,10 @@ export const VNABookingModal = ({
               <Label>Phone/Kakao (không bắt buộc)</Label>
               <Input
                 value={phoneKakao}
-                onChange={(e) => setPhoneKakao(e.target.value)}
+                onChange={(e) => {
+                  const value = e.target.value.replace(/\D/g, ""); // xoá mọi ký tự không phải số
+                  setPhoneKakao(value);
+                }}
                 placeholder="VD: 0901234567"
               />
             </div>
