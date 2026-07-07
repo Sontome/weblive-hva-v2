24c66d5ab040b31c6dfbdd4efca84d37422c84eb
Sontome <93179211+Sontome@users.noreply.github.com>
2026-07-07 23:02:51 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import CheckIn from "./pages/CheckIn";
 import ShiftDashboard from "./pages/ShiftDashboard";
 import CartPage from "./pages/CartPage";
 import HeldTicketsAdminPage from "./pages/HeldTicketsAdminPage";
+import TicketRulesAdmin from "./pages/TicketRulesAdmin";
 
 const queryClient = new QueryClient();
 
@@ -30,6 +31,7 @@ const App = () => (
           <Route path="/checkin" element={<CheckIn />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/admin/held-tickets" element={<HeldTicketsAdminPage />} />
+          <Route path="/admin/ticket-rules" element={<TicketRulesAdmin />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
