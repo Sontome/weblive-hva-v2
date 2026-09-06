95f38d6de1f52e454bff6f6b916b2a13dd4da65d
Sontome <93179211+Sontome@users.noreply.github.com>
2026-09-06 10:59:43 +0700

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -10,7 +10,7 @@ export type Database = {
   // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
-    PostgrestVersion: "13.0.5"
+    PostgrestVersion: "14.5"
   }
   public: {
     Tables: {
@@ -300,6 +300,23 @@ export type Database = {
           vna_threshold_3: number
           vna_threshold_4: number
           vna_threshold_5: number
+          yp_discount_ow_1: number
+          yp_discount_ow_2: number
+          yp_discount_ow_3: number
+          yp_discount_ow_4: number
+          yp_discount_ow_5: number
+          yp_discount_rt_1: number
+          yp_discount_rt_2: number
+          yp_discount_rt_3: number
+          yp_discount_rt_4: number
+          yp_discount_rt_5: number
+          yp_one_way_fee: number
+          yp_round_trip_fee: number
+          yp_threshold_1: number
+          yp_threshold_2: number
+          yp_threshold_3: number
+          yp_threshold_4: number
+          yp_threshold_5: number
         }
         Insert: {
           created_at?: string
@@ -372,6 +389,23 @@ export type Database = {
           vna_threshold_3?: number
           vna_threshold_4?: number
           vna_threshold_5?: number
+          yp_discount_ow_1?: number
+          yp_discount_ow_2?: number
+          yp_discount_ow_3?: number
+          yp_discount_ow_4?: number
+          yp_discount_ow_5?: number
+          yp_discount_rt_1?: number
+          yp_discount_rt_2?: number
+          yp_discount_rt_3?: number
+          yp_discount_rt_4?: number
+          yp_discount_rt_5?: number
+          yp_one_way_fee?: number
+          yp_round_trip_fee?: number
+          yp_threshold_1?: number
+          yp_threshold_2?: number
+          yp_threshold_3?: number
+          yp_threshold_4?: number
+          yp_threshold_5?: number
         }
         Update: {
           created_at?: string
@@ -444,6 +478,23 @@ export type Database = {
           vna_threshold_3?: number
           vna_threshold_4?: number
           vna_threshold_5?: number
+          yp_discount_ow_1?: number
+          yp_discount_ow_2?: number
+          yp_discount_ow_3?: number
+          yp_discount_ow_4?: number
+          yp_discount_ow_5?: number
+          yp_discount_rt_1?: number
+          yp_discount_rt_2?: number
+          yp_discount_rt_3?: number
+          yp_discount_rt_4?: number
+          yp_discount_rt_5?: number
+          yp_one_way_fee?: number
+          yp_round_trip_fee?: number
+          yp_threshold_1?: number
+          yp_threshold_2?: number
+          yp_threshold_3?: number
+          yp_threshold_4?: number
+          yp_threshold_5?: number
         }
         Relationships: []
       }
@@ -956,12 +1007,12 @@ export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
+  TableName extends (DefaultSchemaTableNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
     ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never) = never,
 > = DefaultSchemaTableNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
@@ -985,11 +1036,11 @@ export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
+  TableName extends (DefaultSchemaTableNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never) = never,
 > = DefaultSchemaTableNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
@@ -1010,11 +1061,11 @@ export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
-  TableName extends DefaultSchemaTableNameOrOptions extends {
+  TableName extends (DefaultSchemaTableNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never) = never,
 > = DefaultSchemaTableNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
@@ -1035,11 +1086,11 @@ export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof DatabaseWithoutInternals },
-  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+  EnumName extends (DefaultSchemaEnumNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never) = never,
 > = DefaultSchemaEnumNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
@@ -1052,11 +1103,11 @@ export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof DatabaseWithoutInternals },
-  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+  CompositeTypeName extends (PublicCompositeTypeNameOrOptions extends {
     schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never,
+    : never) = never,
 > = PublicCompositeTypeNameOrOptions extends {
   schema: keyof DatabaseWithoutInternals
 }
